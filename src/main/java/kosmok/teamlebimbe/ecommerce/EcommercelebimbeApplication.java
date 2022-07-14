d76d1ba45483aaec88d7ef88e645d111a3a0fcba
Andrea Scorza <Andreascorza85@gmail.com>
2022-07-14 11:06:42 +0200

    First commit

---
+++ src/main/java/kosmok/teamlebimbe/ecommerce/EcommercelebimbeApplication.java
@@ -0,0 +1,13 @@
+package kosmok.teamlebimbe.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercelebimbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercelebimbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kosmok/teamlebimbe/ecommerce/EcommercelebimbeApplicationTests.java
@@ -0,0 +1,13 @@
+package kosmok.teamlebimbe.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercelebimbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
